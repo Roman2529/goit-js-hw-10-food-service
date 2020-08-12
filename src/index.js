763ad7f5742f8fd4ea4253412d5c34fabb0b6d50
Roman Demikhov <demikhovroman@gmail.com>
2020-08-12 18:50:06 +0300

    fix code, add 3 dope function

--- src/index.js
+++ src/index.js
@@ -1,39 +1,48 @@
-// import './styles.css';
-// import menu from './menu.json';
-// import menuTemplate from './menu-items.hbs';
+import './styles.css';
+import menu from './menu.json';
+import menuTemplate from './menu-items.hbs';
 
-// const menuDraft = { menu: menu };
+const menuDraft = { menu: menu };
 
-// const theme = {
-//     LIGHT: 'light-theme',
-//     DARK: 'dark-theme',
-// };
-// const refs = {
-//     body: document.querySelector('body'),
-//     menuList: document.querySelector('ul.js-menu'),
-//     themeCheckbox: document.querySelector('#theme-switch-control')
-// };
-// const setTheme = () => {
-//     if (localStorage.getItem('theme') === theme.DARK) {
-//         refs.themeCheckbox.checked = true;
-//         refs.body.classList.add(theme.DARK);
-//     }
-// };
+const theme = {
+    LIGHT: 'light-theme',
+    DARK: 'dark-theme',
+};
+const refs = {
+    body: document.querySelector('body'),
+    menuList: document.querySelector('ul.js-menu'),
+    themeCheckbox: document.querySelector('#theme-switch-control')
+};
+const setTheme = () => {
+    if (localStorage.getItem('theme') === theme.DARK) {
+        refs.themeCheckbox.checked = true;
+        refs.body.classList.add(theme.DARK);
+    }
+};
 
-// const themeChange = () => {
-//     if (refs.themeCheckbox.checked) {
-//         refs.body.classList.remove(theme.LIGHT);
-//         refs.body.classList.add(theme.DARK);
-//         localStorage.setItem('theme', theme.DARK);
-//     } else {
-//         refs.body.classList.remove(theme.DARK);
-//         refs.body.classList.add(theme.LIGHT);
-//         localStorage.setItem('theme', theme.LIGHT);
-//     }
-// };
+const themeChange = () => {
+    if (refs.themeCheckbox.checked) {
+        removeClass(theme.LIGHT);
+        addClass(theme.DARK);
+        sendData('theme', theme.DARK);
+    } else {
+        removeClass(theme.DARK);
+        addClass(theme.LIGHT);
+        sendData('theme', theme.LIGHT);
+    }
+};
+let sendData = (key, data) => {
+    localStorage.setItem(key, data);
+};
+let addClass = (key, data) => {
+    refs.body.classList.add(key, data);
+};
+let removeClass = (key, data) => {
+    refs.body.classList.remove(key, data);
+};  
 
-// setTheme();
-// refs.themeCheckbox.addEventListener('change', themeChange);
-// refs.menuList.insertAdjacentHTML('beforeend', menuTemplate(menuDraft))
+setTheme();
+refs.themeCheckbox.addEventListener('change', themeChange);
+refs.menuList.insertAdjacentHTML('beforeend', menuTemplate(menuDraft))
 
 
